bd5257cbb4473d9ca7fa098396a706bddf696aed
Vivian de Carvalho Rodrigues <150278683+vcarvalhorj@users.noreply.github.com>
2025-07-30 18:30:06 -0300

    Update model.py

--- experiments/ckan_rock_experiment3/rock_seg/model.py
+++ experiments/ckan_rock_experiment3/rock_seg/model.py
@@ -2,36 +2,43 @@
 import torch
 import torch.nn as nn
 from rock_seg.kan_conv import KAN_Convolutional_Layer   #from rock_seg.kan_conv import KAN_Convolutional_Layer
+from rock_seg.config import CKAN_CONFIG
 
 class CKANConv2DReal(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation):
         super().__init__()
         self.kan_conv = KAN_Convolutional_Layer(
             in_channels=in_channels,
             out_channels=out_channels,
-            kernel_size=(3,3),
-            stride=(1,1),
-            padding=(1,1),
-            dilation=(1,1),
-            #base_activation=nn.SiLU
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
         )
 
     def forward(self, x):
         return self.kan_conv(x)
 
 class CKANSegmentationModel(nn.Module):
-    def __init__(self, num_classes=3):
+    def __init__(self):
         super().__init__()
-        self.ckan1 = CKANConv2DReal(1, 16)
+        ch = CKAN_CONFIG["channels"]
+        ks = CKAN_CONFIG["kernel_size"]
+        st = CKAN_CONFIG["stride"]
+        pad = CKAN_CONFIG["padding"]
+        dil = CKAN_CONFIG["dilation"]
+        nc = CKAN_CONFIG["num_classes"]
+
+        self.ckan1 = CKANConv2DReal(ch[0], ch[1], ks, st, pad, dil)
         self.pool1 = nn.Identity()
 
-        self.ckan2 = CKANConv2DReal(16, 32)
+        self.ckan2 = CKANConv2DReal(ch[1], ch[2], ks, st, pad, dil)
         self.pool2 = nn.Identity()
 
-        self.ckan3 = CKANConv2DReal(32, 64)
+        self.ckan3 = CKANConv2DReal(ch[2], ch[3], ks, st, pad, dil)
         self.pool3 = nn.Identity()
 
-        self.final_conv = nn.Conv2d(64, num_classes, kernel_size=1)
+        self.final_conv = nn.Conv2d(ch[3], nc, kernel_size=1)
 
     def forward(self, x):
         x = self.pool1(self.ckan1(x))
