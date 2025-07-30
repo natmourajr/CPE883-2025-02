0a2af325849221da38daa4c2610947a675c168cf
Vivian de Carvalho Rodrigues <150278683+vcarvalhorj@users.noreply.github.com>
2025-07-30 18:27:02 -0300

    Update main.py

--- experiments/ckan_rock_experiment3/main.py
+++ experiments/ckan_rock_experiment3/main.py
@@ -4,7 +4,10 @@ import torch
 from torch.utils.data import DataLoader
 from rock_seg_loader.rock_dataset_multi import RockSegmentationDatasetMulti
 from rock_seg.model import CKANSegmentationModel
+from rock_seg.config import CKAN_CONFIG
 from torch import nn, optim
+import matplotlib
+matplotlib.use('TkAgg')  # Usa backend não-interativo compatível com servidores/headless
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
@@ -12,16 +15,23 @@ app = typer.Typer()
 
 @app.command()
 def train(
-    data_dir: str = r"C:\Users\User\Documents\Python Scripts\mini_dataset",
+    data_dir: str = r"C:\Users\vrodrigues\Documents\Python Scripts\mini_dataset_2",
     epochs: int = 5,
-    batch_size: int = 2,
+    batch_size: int = 1,
     lr: float = 1e-3
 ):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataset = RockSegmentationDatasetMulti(data_dir, output_shape=(128, 128))
+
+    # Diagnóstico:
+    if device.type == "cuda":
+        torch.cuda.empty_cache()
+        print("📊 Diagnóstico de memória CUDA antes do carregamento do modelo/dados:")
+        print(torch.cuda.memory_summary(device=device, abbreviated=False))
+
+    dataset = RockSegmentationDatasetMulti(data_dir, output_shape=CKAN_CONFIG["output_shape"])  #dataset = RockSegmentationDatasetMulti(data_dir, output_shape=(128, 128))
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    model = CKANSegmentationModel(num_classes=3).to(device)
+    model = CKANSegmentationModel().to(device)                                                  #model = CKANSegmentationModel(num_classes=3).to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
     losses = []
@@ -35,7 +45,7 @@ def train(
 
         pbar = tqdm(dataloader, desc=f"Epoch {epoch+1}/{epochs}")
         for imgs, masks, _ in pbar:
-            print(f"Batch imgs shape: {imgs.shape}, masks shape: {masks.shape}")
+            #print(f"Batch imgs shape: {imgs.shape}, masks shape: {masks.shape}")
             imgs, masks = imgs.to(device), masks.to(device)
             preds = model(imgs)
             loss = criterion(preds, masks)
