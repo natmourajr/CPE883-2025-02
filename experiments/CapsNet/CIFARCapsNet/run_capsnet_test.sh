9a446ec62adea4890833c11782d23e794ef4fca7
czewski <eduardohb10@hotmail.com>
2025-08-11 15:50:25 -0300

    feat: experiment for cifar using capsnet

---
+++ experiments/CapsNet/CIFARCapsNet/run_capsnet_test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Run CapsNet testing on CIFAR-10
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CAPSNET_SCRIPT="$SCRIPT_DIR/cifar_capsnet.py"
+DATA_DIR="../../../datasets/cifar10"
+SAVE_DIR="$SCRIPT_DIR/result"
+WEIGHTS="$SAVE_DIR/epoch8.pkl"  # Change if you want to test a different weights file
+
+# Default arguments
+BATCH_SIZE=100
+LR=0.001
+LR_DECAY=0.9
+LAM_RECON=1.536  # 0.0005*3072 = 3*32*32 for CIFAR-10
+ROUTINGS=3
+
+# Create result directory if it doesn't exist
+mkdir -p "$SAVE_DIR"
+
+# Run testing
+python3 "$CAPSNET_SCRIPT" \
+    --batch_size $BATCH_SIZE \
+    --lr $LR \
+    --lr_decay $LR_DECAY \
+    --lam_recon $LAM_RECON \
+    --routings $ROUTINGS \
+    --data_dir "$DATA_DIR" \
+    --save_dir "$SAVE_DIR" \
+    --testing \
+    --weights "$WEIGHTS"
