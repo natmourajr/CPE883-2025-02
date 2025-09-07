be0e7aa8ff8eae6d65ee2e8ed1bbeceb2fb09dcd
czewski <eduardohb10@hotmail.com>
2025-09-07 16:09:59 -0300

    feat: cifar100 dataloaders

--- dataloaders/cifar_dataloaders/cifar_dataloaders/__init__.py
+++ dataloaders/cifar_dataloaders/cifar_dataloaders/__init__.py
@@ -1,4 +1,4 @@
 # Import the main class from the module
-from .cifar import CIFAR10Dataset
+from .cifar import CIFAR10Dataset, CIFAR100Dataset
 
-__all__ = ["CIFAR10Dataset"]
+__all__ = ["CIFAR10Dataset", "CIFAR100Dataset"]
